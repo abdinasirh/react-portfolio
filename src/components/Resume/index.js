c938fdeecaaf78df454e8fdf87a4414920eec9c1
Abdinasir Hassan <abdinasirduud20@gmail.com>
2023-11-07 09:08:28 -0600

    fixed resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -9,7 +9,7 @@ function Resume() {
 
         <a
           className="button is-primary"
-          href={process.env.PUBLIC_URL + "/mMyResume.pdf"}
+          href={process.env.PUBLIC_URL + "/MyResume.pdf"}
           target="_blank" rel="noreferrer"
         >
           <span className="icon">
